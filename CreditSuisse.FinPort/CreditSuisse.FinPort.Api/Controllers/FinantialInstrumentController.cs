c48675ddb07c168c03b1dfc958508701e37fe104
yurifontoura <yurifontoura@hotmail.com>
2024-03-01 11:22:44 -0300

    Web api
    Finantial Instrument controller implemented.

---
+++ CreditSuisse.FinPort/CreditSuisse.FinPort.Api/Controllers/FinantialInstrumentController.cs
@@ -0,0 +1,22 @@
+﻿using CreditSuisse.FinPort.Application.Interface.Applications;
+using CreditSuisse.FinPort.Application.Interface.DTO;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CreditSuisse.FinPort.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FinantialInstrumentController(IFinInstrumentApp app) : Controller
+    {
+        private readonly IFinInstrumentApp _app = app;
+
+        [HttpPost(Name = "Categorize")]
+        public async Task<JsonResult> Categorize(FinantialInstrumentDTO[] finantialInstrument)
+        {
+            Envelope<string[]> envelope = await _app.Categorize(finantialInstrument);
+
+            return Json(envelope);
+        }
+    }
+}
